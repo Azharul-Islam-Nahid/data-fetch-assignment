46a476172bf912a07ca29e35866599dfba0516a3
Azharul Islam <lonewolfazharul@gmail.com>
2023-02-01 21:03:37 +0600

    layout and route setup completed

---
+++ src/Components/TableData/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TableData from '../TableData';
+
+const Home = () => {
+    return (
+        <div>
+            <TableData />
+        </div>
+    );
+};
+
+export default Home;
